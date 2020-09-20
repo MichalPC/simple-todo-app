3476603d8bba0c400f7b1f92831a8f9f07b232a2
MichalPC <zfac211@rhul.ac.uk>
2020-09-20 16:51:26 +0100

    Wrap noteText in its own div

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -6,7 +6,7 @@ class Note extends Component {
     render() {
         return (
             <div className="note-container">
-                {this.props.noteText}
+                <div className="noteText">{this.props.noteText}</div>
 
                 <div id className="deleteBtn" onClick={this.props.deleteNote}>
 
