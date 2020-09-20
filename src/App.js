80680f6262d336b25d946b5693f6bdfa301f9b6d
MichalPC <zfac211@rhul.ac.uk>
2020-09-20 15:18:16 +0100

    Add main container and a header

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="main-container">
+                <div className="header"> Simple To-Do App </div>
+            </div>
+        )
+    }
+}
+
+export default App;
