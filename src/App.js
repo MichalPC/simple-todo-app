b1e1a6c5970a6961c072c76ca2f1485020fba2c0
MichalPC <zfac211@rhul.ac.uk>
2020-09-20 15:42:29 +0100

    Add constructor to App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+    constructor(props) {
+        super();
+        this.state = {
+            noteText:'',
+        }
+    }
+
     render() {
         return (
             <div className="main-container">
